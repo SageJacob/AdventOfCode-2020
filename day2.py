16697d4e36a6cf58683451d8fa75c57a0fe0b460
Jacob Sage <46393418+SageJacob@users.noreply.github.com>
2020-12-02 00:30:04 -0500

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,33 @@
+x = open('input2.txt', 'r')
+
+def part1():
+    for line in x:
+        string = line.split(' ')
+        boundaries = string[0].split('-')
+        low, hi = int(boundaries[0]), int(boundaries[1])
+        letter = string[1][0]
+        curr = 0
+        for i in string[2]:
+            if i == letter:
+                print(f'here: {i, letter}')
+                curr += 1
+        if curr >= low and curr <= hi:
+            print(low, hi, curr)
+            valid += 1
+    return valid
+
+def part2(x):
+    valid = 0
+    for line in x:
+        string = line.split(' ')
+        boundaries = string[0].split('-')
+        low, hi = int(boundaries[0]), int(boundaries[1])
+        letter = string[1][0]
+        count = 0
+        if string[2][low-1] == letter:
+            count += 1
+        if string[2][hi-1] == letter:
+            count += 1
+        if count == 1:
+            valid += 1
+    return valid
